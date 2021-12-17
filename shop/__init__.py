b2ed79184d50cedf12bfaeae18a668e6992f3c0f
awg001 <vendetta-chechen@bk.ru>
2021-12-17 22:29:50 +0300

    deny pro

--- shop/__init__.py
+++ shop/__init__.py
@@ -8,8 +8,10 @@ from config import *
 app = Flask(__name__)
 db = SQLAlchemy(app)
 app.config.from_object(Config)
-migrate = Migrate(app,db)
+migrate = Migrate(app, db)
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
+login_manager.login_message = 'Для доступа к данной странице нужно войти в свой аккаунт!'
+login_manager.login_message_category = 'info'
 
 from shop import routes, models
